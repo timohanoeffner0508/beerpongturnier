9167dd37599fb5c7dd49ad169ebb92aab4a2fc27
timohanoeffner0508 <108334285+timohanoeffner0508@users.noreply.github.com>
2025-07-21 16:56:43 +0200

    Initial commit BeerpongLJ

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  res.status(404).send('Seite nicht gefunden.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Beerpongturnier läuft auf Port ${PORT}`);
+});
